2e6a51879630f1b1c5150961f8cfca1632ff9005
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-24 20:17:29 +0200

    hyper-pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -71,7 +71,7 @@ class Server:
             "page_size": page_size,
             "page": page,
             "data": page_data,
-            "next_page": page + 1 if page < page_size else None,
+            "next_page": page + 1 if page < total_pages else None,
             "previous_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
         }
